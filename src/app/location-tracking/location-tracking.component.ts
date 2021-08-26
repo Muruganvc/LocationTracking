30683b8cba8b021b2aaa2d7c509521d1d2f29ee0
Muruganvc <vcmuruganmca@gmail.com>
2021-08-26 23:40:20 +0530

    Changes

--- src/app/location-tracking/location-tracking.component.ts
+++ src/app/location-tracking/location-tracking.component.ts
@@ -20,7 +20,6 @@ export class LocationTrackingComponent implements OnInit {
   ngOnInit(): void {
     this.http.getIPAddress().subscribe(result => {
       this.ipAddress = result;
-      console.log(result);
     })
     this.getGeoLocation();
   }
